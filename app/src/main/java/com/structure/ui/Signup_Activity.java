28c9c1ce7db7a333b4bee3a4a52aebc3f1d8424c
Webmobsoft <nitin1353@gmail.com>
2018-03-13 15:42:15 +0530

    Commit deepika_1

--- app/src/main/java/com/structure/ui/Signup_Activity.java
+++ app/src/main/java/com/structure/ui/Signup_Activity.java
@@ -3,7 +3,7 @@ package com.structure.ui;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.structure.R;
+
 
 public class Signup_Activity extends AppCompatActivity {
 
